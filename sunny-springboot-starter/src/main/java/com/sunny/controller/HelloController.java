53b79ed01d2adf40e9c07a17645a8b73f27dee86
sunlei <949792381@qq.com>
2019-08-28 23:06:21 +0800

    springboot加载资源配置文件

--- sunny-springboot-starter/src/main/java/com/sunny/controller/HelloController.java
+++ sunny-springboot-starter/src/main/java/com/sunny/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.sunny.controller;
 
+import com.sunny.model.JSONResult;
+import com.sunny.model.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Resource resource;
+
     @RequestMapping("/hello")
     public Object hello()
     {
         return "Hello springboot~~";
+    }
+
+
+    @RequestMapping("/getResource")
+    public JSONResult getResource()
+    {
+        Resource bean = new Resource();
+        BeanUtils.copyProperties(resource,bean);
+
+        return JSONResult.ok(bean);
     }
 }
---
+++ sunny-springboot-starter/src/main/java/com/sunny/model/Resource.java
@@ -0,0 +1,42 @@
+package com.sunny.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by sunlei on 2019/8/28.
+ */
+@Configuration
+@ConfigurationProperties(prefix = "com.sunny.opensource")
+@PropertySource(value = "classpath:resource.properties")
+public class Resource {
+
+    private String name;
+    private String website;
+    private String language;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
